dd8a57a0205edd28bed86a19af919f7c80ff3c30
FloreauLuca <lucafloreau@akeonet.com>
2020-06-20 19:40:01 +0200

    Add Obstacle parameters and add can

--- Assets/Scripts/Environment/Obstacle.cs
+++ Assets/Scripts/Environment/Obstacle.cs
@@ -6,6 +6,13 @@ public class Obstacle : MonoBehaviour
 {
     private Rigidbody rigidbody;
     [SerializeField] private float forceSpeed;
+    [SerializeField] private float heightSpeed;
+    [SerializeField] private float screenAmplitudeGain = 10f;
+    [SerializeField] private float screenFrequencyGain = 10f;
+    [SerializeField] private float carAmplitudeGain = 5f;
+    [SerializeField] private float carFrequencyGain = 50f;
+    [SerializeField] private float screenshakeDuration = 1f;
+    [SerializeField] private float carshakeDuration = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +29,12 @@ public class Obstacle : MonoBehaviour
     {
         if (other.gameObject.GetComponent<CarMovement>())
         {
-            rigidbody.AddForce((transform.position - other.transform.position + Vector3.up) * forceSpeed * other.relativeVelocity.magnitude);
+            rigidbody.AddForce((transform.position - other.transform.position) * forceSpeed * other.relativeVelocity.magnitude + Vector3.up * heightSpeed );
             //other.gameObject.GetComponent<CarMovement>().BumpObstacle();
             if (other.gameObject.CompareTag("Player"))
             {
-                other.gameObject.GetComponent<PlayerController>().ScreenShake();
-                other.gameObject.GetComponent<PlayerController>().CarShake();
+                other.gameObject.GetComponent<PlayerController>().ScreenShake(screenAmplitudeGain, screenFrequencyGain, screenshakeDuration);
+                other.gameObject.GetComponent<PlayerController>().CarShake(carAmplitudeGain, carFrequencyGain, carshakeDuration);
             }
         }
     }
