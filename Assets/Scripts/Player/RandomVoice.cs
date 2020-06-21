0dfe3ecba0c8b85d9d3160508794bad745d5bd62
surue <nicolas06062000@hotmail.com>
2020-06-21 17:43:01 +0200

    Add random voice to player

---
+++ Assets/Scripts/Player/RandomVoice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomVoice : MonoBehaviour {
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private List<AudioClip> voices;
+    [SerializeField] private float minTime = 20;
+    [SerializeField] private float maxTime = 40;
+
+    private float timer = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(minTime, maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            audioSource.clip = voices[Random.Range(0, voices.Count)];
+            audioSource.Play();
+            
+            timer = Random.Range(minTime, maxTime);
+        }
+    }
+}
